96a18064031534799d210bdf967bc6f86b924ef7
Caleb Connolly <caleb.connolly@linaro.org>
2023-12-11 07:40:27 +0000

    gpio/qcom_pmic: add pm7325
    Add a compatible for pm7325 PMIC
    
    Signed-off-by: Caleb Connolly <caleb.connolly@linaro.org>

--- drivers/gpio/qcom_pmic_gpio.c
+++ drivers/gpio/qcom_pmic_gpio.c
@@ -349,6 +349,7 @@ static const struct udevice_id qcom_gpio_ids[] = {
 	{ .compatible = "qcom,pm6125-gpio" },
 	{ .compatible = "qcom,pm8150-gpio" },
 	{ .compatible = "qcom,pm8550-gpio" },
+	{ .compatible = "qcom,pm7325-gpio" },
 	{ }
 };
 
