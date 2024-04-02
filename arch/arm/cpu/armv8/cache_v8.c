2da662d66f142b5c12a0002b50018fcee4b45288
Caleb Connolly <caleb.connolly@linaro.org>
2024-04-02 10:16:21 +0200

    HACK: arch: armv8: catch borked calls to fluch/invalidate dcache
    The dwc3 stack seems to occasionally call flush/invalidate dcache
    helpers with a start address of 0x0, end 0x40. Until I figure out what's
    going wrong here, catch these and complain about it.
    
    Signed-off-by: Caleb Connolly <caleb.connolly@linaro.org>

--- arch/arm/cpu/armv8/cache_v8.c
+++ arch/arm/cpu/armv8/cache_v8.c
@@ -787,6 +787,10 @@ inline void flush_dcache_all(void)
  */
 void invalidate_dcache_range(unsigned long start, unsigned long stop)
 {
+	if (!start || !stop) {
+		printf("NULL %s: %#lx - %#lx\n", __func__, start, stop);
+		return;
+	}
 	__asm_invalidate_dcache_range(start, stop);
 }
 
@@ -795,6 +799,10 @@ void invalidate_dcache_range(unsigned long start, unsigned long stop)
  */
 void flush_dcache_range(unsigned long start, unsigned long stop)
 {
+	if (!start || !stop) {
+		printf("NULL %s: %#lx - %#lx\n", __func__, start, stop);
+		return;
+	}
 	__asm_flush_dcache_range(start, stop);
 }
 #else
